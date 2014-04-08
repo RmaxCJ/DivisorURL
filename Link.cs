390c9617e709ac77bf9d1ee9faac24665cf53eee
m <m@m.(none)>
2014-04-07 20:36:38 -0500

    PrimeraVez

---
+++ Link.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace DivisorURL
+{
+    class Link
+    {
+
+                      private String url;
+                private String protocolo;
+                private String dominio;
+                private String ruta;
+
+                public Link(String s)
+                    {
+                    this.url=s;
+                    }
+        
+                public void Separa()
+                {
+
+
+                    String[] arr;
+                    if(url.Contains("//"))
+                    {
+                        arr = Regex.Split(url, ":");
+                        this.protocolo = arr[0];
+                        arr = Regex.Split(arr[1], "//");
+                        arr = Regex.Split(arr[1], "/");
+                        this.dominio = arr[0];
+                        this.ruta = "";
+
+                        for(int i =1;i<=arr.Length-1;i++)
+                        {
+                            this.ruta += "/" +arr[i];
+                        }
+
+
+                    }
+                    else
+                    {
+                        if(url.Contains("/"))
+                            {
+                                arr= Regex.Split(url,"/");
+                                this.protocolo= "NO HAY PROTOCOLO";
+                                this.dominio= arr[0];
+                                this.ruta="";
+                                    for (int i = 1; i <=arr.Length; i++)
+			                                    {
+			                                        this.ruta+="/"+arr[i];
+			                                    }
+
+                            }
+                            else 
+                            {
+                                this.dominio= this.url;
+                                this.protocolo="NO HAY PROTOCOLO";
+                                this.ruta="NO HAY RUTA";
+                            }
+
+
+                                           // return protocolo;
+
+
+                    }
+                }
+
+              public String getUrl() 
+              {
+                return url;
+              }
+
+                public String getProtocolo() 
+                {
+                    return protocolo;
+                }
+
+                public String getDominio()
+                {
+                    return dominio;
+                }
+
+                public String getRuta() 
+                {
+                    return ruta;
+                }
+              
+
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisorURL
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            String prueba;
+            
+            Console.Write("\tINTRODUCE UNA URL:       ");
+            prueba = Console.ReadLine().ToLower();
+            Link frase = new Link(prueba);
+            
+            Console.WriteLine("");
+            frase.Separa();
+
+            System.Console.WriteLine(" El protocolo es    \t" +frase.getProtocolo());
+            System.Console.WriteLine(" El dominio es    \t" + frase.getDominio());
+            System.Console.WriteLine(" El ruta es    \t" + frase.getRuta());
+
+          
+
+
+            Console.WriteLine("");
+
+            System.Console.WriteLine("Presiona cualquier tecla para salir");
+            System.Console.ReadKey();
+          
+
+        }
+
+     }
+
+}
+
